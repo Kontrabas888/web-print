8c0c90667051c5c52b507dbd1785e4080b5c8b54
Oleksandr_Moroz <110786733+Kontrabas888@users.noreply.github.com>
2024-03-23 01:59:47 -0600

    web-print-v0.2.7

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,23 +1,8 @@
-import { useNavigate, useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-
 const NavigationBar = () => {
-    const navigate = useNavigate();
-
-    const goToHomePage = () => {
-        navigate('*');
-    };
-    
-    const location = useLocation();
-    const shouldShowBackButton = location.pathname !== '/';
-
-
     return (
         <nav>
-            {shouldShowBackButton && (
-                <button className='back printable' onClick={goToHomePage}>На главную</button>
-            )}
             <ul className='bottoms-navigate'>
                 <li className='link-nav printable'><Link to="/main">Стандартные</Link></li>
                 <li className='link-nav printable'><Link to="/shapes">Размер</Link></li>
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
 import App from './App';
 import ShapePage from './ShapePage';
-import InitialGifScreen from './InitialGifScreen';
 import NavigationBar from './NavigationBar';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/web-print">
     <DndProvider backend={HTML5Backend}>
       <Routes>
-        <Route path="*" element={<InitialGifScreen />} />
-        <Route path="/" element={<><NavigationBar /><Outlet /></>} >
-          <Route path="/main" element={<App />} />
-          <Route path="/shapes" element={<ShapePage />} />
+        <Route path="/" element={<Navigate replace to="/main" />} />
+        <Route path="/" element={<><NavigationBar /><Outlet /></>}>
+          <Route path="main" element={<App />} />
+          <Route path="shapes" element={<ShapePage />} />
         </Route>
       </Routes>
     </DndProvider>
